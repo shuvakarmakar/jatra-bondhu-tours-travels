5875d317b3e19f53a05e41fe39df192bbddb88f3
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-10-19 19:57:52 +0600

    React ROuter Dom Installed and Home Component Created

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center text-2xl">This is Home Page</h1>
+            <h3 className="text-lg mx-auto my-auto text-center font-bold">This is a Tours and Travels Website</h3>
+        </div>
+    );
+};
+
+export default Home;    
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "../Pages/Home";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home></Home>,
+    },
+]);
