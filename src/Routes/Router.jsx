b23bbb8eeb3cad4e0672e20a8eb65dbb4db3be62
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-11-14 12:33:04 +0600

    Terms and Conditions component Implemented

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import Home from "../Pages/Home";
+import TermsandConditions from "../Pages/TermsandConditions";
 
 
 export const router = createBrowserRouter([
@@ -9,4 +10,8 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
     },
+    {
+        path: "terms-and-conditions",
+        element: <TermsandConditions></TermsandConditions>
+    }
 ]);
