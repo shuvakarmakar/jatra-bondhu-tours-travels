92d89bc8b36e959427dfadb1ae6e22f6683b2eeb
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-12-30 18:53:06 +0600

    Sidebar and Dashboard added for admin, and customer

---
+++ src/Layout/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Components/Dashboard/Sidebar";
+import Footer from "../Sections/Footer";
+
+const Dashboard = () => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <div className="flex flex-grow">
+                {/* Sidebar */}
+                <div className="hidden md:block md:w-2/12 bg-gray-100">
+                    <Sidebar />
+                </div>
+                {/* Main Content */}
+                <div className="w-full md:w-10/12 p-4 md:p-12">
+                    <Outlet />
+                </div>
+            </div>
+            {/* Footer */}
+            {/* <Footer /> */}
+        </div>
+    );
+};
+
+export default Dashboard;
